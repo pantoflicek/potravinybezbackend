bbe458944f5208011cf79bd18e7221e39e32a950
jakub <jakub@littleslipper.cz>
2023-02-09 19:14:12 +0100

    initial commit

---
+++ src/main/java/cz/vse/potravinyBEZ/PotravinyBezApplication.java
@@ -0,0 +1,13 @@
+package cz.vse.potravinyBEZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotravinyBezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotravinyBezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/vse/potravinyBEZ/PotravinyBezApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vse.potravinyBEZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotravinyBezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
