ad99d50020bfe3758136acc45b5725483efa6fb3
jakub <jakub@littleslipper.cz>
2023-02-16 18:51:49 +0100

    producer - controller

---
+++ src/main/java/cz/vse/potravinyBEZ/controller/ProducerController.java
@@ -0,0 +1,39 @@
+package cz.vse.potravinyBEZ.controller;
+
+import cz.vse.potravinyBEZ.domain.producer.*;
+import cz.vse.potravinyBEZ.service.ProducerService;
+
+//Persistence
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+//Lombok
+import lombok.AllArgsConstructor;
+
+//Spring
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("producers")
+@CrossOrigin(origins = "http://localhost:3000/")
+@AllArgsConstructor
+public class ProducerController {
+    private final ProducerService producerService;
+
+    @PostMapping
+    public CreateProducerResponse createProducer(@Valid @NonNull @RequestBody CreateProducerRequest createProducerRequest){
+        return producerService.createProducer(createProducerRequest);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public DeleteProducerResponse deleteProducer(@Valid @NonNull @RequestBody DeleteProducerRequest deleteProducerRequest){
+        return producerService.deleteProducerByName(deleteProducerRequest);
+    }
+
+    @GetMapping
+    public GetAllProducersResponse getAllProducers(){
+        return producerService.getAllProducers();
+    }
+}
