b09abce3c1ce9433fcffe88aaa659803f61f804e
jakub <jakub@littleslipper.cz>
2023-02-16 11:25:22 +0100

    role - service impl

---
+++ src/main/java/cz/vse/potravinyBEZ/repository/entity/UserRoleEntity.java
@@ -0,0 +1,27 @@
+package cz.vse.potravinyBEZ.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_role")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRoleEntity {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private RoleEntity role;
+}
