6f5d113a25c7535d614f7ce3e54b5d23380dff9c
jakub <jakub@littleslipper.cz>
2023-03-04 13:33:03 +0100

    spring security

---
+++ src/main/java/cz/vse/potravinyBEZ/domain/security/AccessToken.java
@@ -0,0 +1,27 @@
+package cz.vse.potravinyBEZ.domain.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//Lombok
+import lombok.*;
+
+//Java
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessToken {
+    private String subject;
+    private List<String> roles;
+    private String userId;
+
+    @JsonIgnore
+    public boolean hasRole(String roleName) {
+        if (roles == null) {
+            return false;
+        }
+        return roles.contains(roleName);
+    }
+}
